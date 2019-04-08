5370646834a5caf1d4eaa5dacdb6eeafce7f9708
dazingwill <spumirant@aliyun.com>
2019-04-08 01:22:03 -0700

    change details, add comments

--- src/data_table.py
+++ src/data_table.py
@@ -1,30 +1,47 @@
 import csv
+from collections.abc import Iterable
 
 
 class DataTable:
+    """
+    General table structure with lines and headers
+
+    self.headers: a list of header str
+    self.records: iterable object, elements type is list. Expected used only once.
+
+    Usage:
+    import data form csv file: table = DataTable.from_csv(csv_file)
+    save this table to csv file: table.to_csv(filepath)
+    iter records: for row in table
+    iter records in dict format: for row in table.dict_records()
+    """
+
+    # if PREDEFINED_HEADERS is defined,
+    # the headers in this table must include all the fields PREDEFINED_HEADERS have.
     PREDEFINED_HEADERS = None
 
     def is_headers_compatible(self, headers):
         if self.PREDEFINED_HEADERS is None or headers is None:
-            return True
+            return self.PREDEFINED_HEADERS or headers
         missed_headers = set(self.PREDEFINED_HEADERS).difference(headers)
         return len(missed_headers) == 0
 
     def __init__(self, records=None, headers=None):
         if not self.is_headers_compatible(headers):
-            raise AttributeError("Cannot find all required headers: "
-                                 "headers must include {}".format(", ".join(self.PREDEFINED_HEADERS)))
+            raise ValueError("Cannot find all required headers: "
+                             "headers must include {}".format(", ".join(self.PREDEFINED_HEADERS)))
+        if not isinstance(records, Iterable):
+            raise TypeError("records must be iterable")
 
         self.headers = headers or self.PREDEFINED_HEADERS
-        self.header_index = {header: index for index, header in enumerate(self.headers)}
-
         self.records = records
 
-    def load_csv(self, csv_file):
+    @classmethod
+    def from_csv(cls, csv_file):
         csv_records = csv.reader(csv_file)
         headers = next(csv_records)
-        self.__init__(csv_records, headers)
-        return self
+        table = cls(csv_records, headers)
+        return table
 
     def to_csv(self, filepath):
         with open(filepath, "w", newline="") as csv_file:
@@ -35,7 +52,7 @@ class DataTable:
     def dict_records(self):
         return map(lambda row: {
             self.headers[i]: row[i]
-            for i in range(len(self.headers))
+            for i in range(min(len(self.headers), len(row)))
         }, self.records)
 
     def __iter__(self):
@@ -43,8 +60,12 @@ class DataTable:
 
 
 class OrderProductTable(DataTable):
+    """
+    Table class for order records
+    """
     PREDEFINED_HEADERS = ["order_id", "product_id", "add_to_cart_order", "reordered"]
 
+    # Note: hardcoded this transfer function to make it faster
     def dict_records(self):
         return map(lambda row: {
             "product_id": row[1],
@@ -53,14 +74,25 @@ class OrderProductTable(DataTable):
 
 
 class ProductTable(DataTable):
+    """
+    Table class for product records
+    """
     PREDEFINED_HEADERS = ["product_id", "product_name", "aisle_id", "department_id"]
 
 
 class DepartmentStaticsTable(DataTable):
+    """
+    Table class for department statics
+
+    to create a blank department statics table:
+    table = DepartmentStaticsTable(departments=possible_department_ids)
+
+    the records is usually in dict format, only calculate ratio and reorder before print to csv
+    """
     PREDEFINED_HEADERS = ["department_id", "number_of_orders", "number_of_first_orders", "percentage"]
 
     def __init__(self, records=None, departments=None):
-        if not records and departments:
+        if not records and departments is not None:
             records = {department_id: [department_id, 0, 0, 0] for department_id in departments}
         super().__init__(records)
 
@@ -73,6 +105,8 @@ class DepartmentStaticsTable(DataTable):
     def summarize(self):
         if isinstance(self.records, dict):
             self.records = self.records.values()
+
+        # remove the rows with no order count
         self.records = filter(lambda record: record[1] > 0, self.records)
 
         def calculate_ratio(row):
@@ -80,8 +114,10 @@ class DepartmentStaticsTable(DataTable):
             return row
         self.records = map(calculate_ratio, self.records)
 
+        # put rows in increasing order of department's id
         self.records = sorted(self.records, key=lambda row: int(row[0]))
         return self.records
 
-
-
+    def to_csv(self, filepath):
+        self.summarize()
+        super().to_csv(filepath)
--- src/purchase_analytics.py
+++ src/purchase_analytics.py
@@ -1,4 +1,3 @@
-import time
 import sys
 import os
 
@@ -6,68 +5,86 @@ from data_table import DepartmentStaticsTable, OrderProductTable, ProductTable
 
 
 def extract_product_departments(products_path):
+    """
+    Extract products' department id from csv file
+
+    :param products_path: str, path of the csv file holds product records
+    :return: product_departments: dict, map product id to department id
+    """
     with open(products_path, "r", encoding="utf-8") as products_file:
-        product_records = ProductTable().load_csv(products_file)
+        product_records = ProductTable.from_csv(products_file)
+
         product_departments = {row["product_id"]: row["department_id"] for row in product_records.dict_records()}
         return product_departments
 
 
 def count_department_orders(order_products_path, product_departments):
+    """
+    For each departmentCount, count the orders and first orders made for the products in this department
+    and save the numbers in a table.
+
+    :param order_products_path: str, path of the csv file holds order records
+    :param product_departments: dict, map product id to department id
+    :return: department_statics: DepartmentStaticsTable, save the counts of orders for each department
+    """
+
+    # init department statics table
     departments = set(product_departments.values())
     department_statics = DepartmentStaticsTable(departments=departments)
 
+    # iterate order records from csv file and add counts into statics table
     with open(order_products_path, "r", encoding="utf-8") as order_products_file:
-        orders = OrderProductTable().load_csv(order_products_file)
+        orders = OrderProductTable.from_csv(order_products_file)
+
         for row in orders.dict_records():
+            # skip the products of which the department cannot be found
             if row["product_id"] not in product_departments:
                 continue
+
+            # count order for the department
             department_id = product_departments[row["product_id"]]
             department_statics.add_order_count(department_id)
 
+            # count first order for the department
             if not row["reordered"]:
                 department_statics.add_first_order_count(department_id)
 
     return department_statics
 
 
-def write_report(report_path, department_statics):
-    department_statics.summarize()
+def analyze_purchases(order_products_path, products_path, report_path):
+    """
+    Calculate, for each department, the number of times a product was requested,
+    number of times a product was requested for the first time and a ratio of those two numbers.
+    Save the result into a report csv file.
+
+    :param order_products_path: str, path of the csv file holds order records
+    :param products_path: str, path of the csv file holds product records
+    :param report_path: str, path of the report csv file to be created
+    :return: None
+    """
+    product_departments = extract_product_departments(products_path)
+    department_statics = count_department_orders(order_products_path, product_departments)
     department_statics.to_csv(report_path)
 
 
-def analyze_purchases(order_products_path, products_path, report_path):
-    product_departments = extract_product_departments(products_path)
-    department_statics = count_department_orders(order_products_path, product_departments)
-    write_report(report_path, department_statics)
-
-
-order_products_path = "./input/order_products.csv"
-order_products_path = "./input/order_products__train.csv"
-order_products_path = "./input/order_products__prior.csv"
-products_path = "./input/products.csv"
-report_path = "./output/report.csv"
-
 if __name__ == '__main__':
 
-    start_time = time.time()
-
-    if len(sys.argv) < 4:
+    if len(sys.argv) != 4:
         print("Usage: python ./src/purchase_analytics.py "
               "./input/order_products.csv ./input/products.csv "
               "./output/report.csv")
         exit()
 
-    # order_products_path = sys.argv[1]
-    # products_path = sys.argv[2]
-    # report_path = sys.argv[3]
+    order_products_path = sys.argv[1]
+    products_path = sys.argv[2]
+    report_path = sys.argv[3]
+
+    if not os.access(order_products_path, os.R_OK):
+        print("Cannot access file "+order_products_path)
+        exit()
+    if not os.access(products_path, os.R_OK):
+        print("Cannot access file "+products_path)
+        exit()
 
     analyze_purchases(order_products_path, products_path, report_path)
-
-    end_time = time.time()
-    print("program running time: "+str(end_time-start_time))
-
-
-
-
-
-
